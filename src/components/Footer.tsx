15412ec87ef800e4d3a2f1fc35a8f68f39332004
leadgeex <aaronleadgeex@gmail.com>
2022-05-17 23:34:50 +0800

    footer copyright text font-size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="w-full py-6">
             <div className="container max-w-6xl px-8 m-auto text-center md:px-0">
-                <p className="font-mono text-center text-white text-normal">
+                <p className="font-mono text-center text-white text-sm md:text-base">
                     &copy; 2022 <span className="underline">Aaron Carmen</span>.
                     All Rights Reserved.
                 </p>
