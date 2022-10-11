651fcc4db766f5e62b200f678da612c38488243a
leadgeex <aaronleadgeex@gmail.com>
2022-10-11 23:14:17 +0800

    navbar alignment fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,15 @@
 import { FC } from "react";
 
 const Navbar: FC = () => {
-    return (
-        <div className="w-full py-6">
-            <div className="container py-4">
-                <h1 className="font-mono text-2xl font-bold text-gray-400 uppercase text-center">
-                    Aaron Carmen
-                </h1>
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-full py-6">
+      <div className="container py-4 m-auto">
+        <h1 className="font-mono text-2xl font-bold text-center text-gray-400 uppercase">
+          Aaron Carmen
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
